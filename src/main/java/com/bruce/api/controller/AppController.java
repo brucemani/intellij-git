996b4830c5b6029a0c6f86b8ac1fad9f2ca10d11
Bruemani <rajamanikandanreg32@gmail.com>
2023-12-31 20:16:34 +0530

    create-controller commit

--- src/main/java/com/bruce/api/controller/AppController.java
+++ src/main/java/com/bruce/api/controller/AppController.java
@@ -1,7 +1,28 @@
-package com.bruce.api.controller;/**
-    * @Author:  kchid
-    * @Project: Git_practice
-    * @Date: 12/31/2023
-    * @Time: 8:10 PM
-*/public class AppController {
+package com.bruce.api.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.http.ResponseEntity.ok;
+
+/**
+ * @Author: kchid
+ * @Project: Git_practice
+ * @Date: 12/31/2023
+ * @Time: 8:10 PM
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping(path = "/api")
+public class AppController {
+    
+
+    @GetMapping(produces = APPLICATION_JSON_VALUE)
+    public ResponseEntity<?> show() {
+        return ok("Hello mani");
+    }
 }
